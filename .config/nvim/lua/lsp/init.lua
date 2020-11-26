6f381125fc7efbb186d2728238be10290d377252
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-11-25 22:20:32 -0800

    Update neovim lsp settings

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,4 +1,3 @@
-local diagnostic = require('diagnostic')
 local completion = require('completion')
 local lspconfig = require('lspconfig')
 
@@ -9,13 +8,27 @@ local on_attach = function(client, bufnr)
   -- Note these are in on_attach so that they don't override bindings in a non-LSP setting
   vim.fn.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gtd", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gn", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gp", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", {noremap = true, silent = true})
 end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- This will disable virtual text
+    virtual_text = false,
+
+    -- To configure sign display,
+    -- see: ":help vim.lsp.diagnostic.set_signs()"
+    signs = true,
+
+    -- Only update diagnostics after insert
+    update_in_insert = false,
+    }
+)
 
 -- Language server setup
 lspconfig.pyls_ms.setup {
