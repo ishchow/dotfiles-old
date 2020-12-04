8e67735db7afc19972c140f17e90a602b8043849
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-12-03 22:52:09 -0800

    Update lua lsp config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -44,7 +44,28 @@ lspconfig.jsonls.setup {
 }
 
 lspconfig.sumneko_lua.setup {
-    on_attach = on_attach
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Get the language server to recognize LuaJIT globals like `jit` and `bit`
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';'),
+            },
+            diagnostics = {
+            -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                },
+            },
+        },
+    },
 }
 
 lspconfig.vimls.setup {
